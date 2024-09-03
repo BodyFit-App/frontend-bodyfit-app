4ab3f369518871f2828ac976f0ec6a7a6a724de8
stephane0713 <stephaned2205@gmail.com>
2024-09-03 18:25:26 +0200

    feature: added Theme provider

--- screens/index.tsx
+++ screens/index.tsx
@@ -5,21 +5,28 @@ import { HomeScreen } from "./home";
 import { ExerciseScreen } from "./exercise";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ExercisesScreen } from "./exercises";
+import { MD3DarkTheme, Provider as PaperProvider } from "react-native-paper";
+
+const theme = {
+  ...MD3DarkTheme,
+};
 
 const queryClient = new QueryClient();
 
 const Screens = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <NativeRouter>
-        <Routes>
-          <Route path="/" element={<HomeScreen />} />
-          <Route path="/login" element={<LoginScreen />} />
-          <Route path="/exercises" element={<ExercisesScreen />} />
-          <Route path="/exercise/:id" element={<ExerciseScreen />} />
-        </Routes>
-      </NativeRouter>
-    </QueryClientProvider>
+    <PaperProvider theme={theme}>
+      <QueryClientProvider client={queryClient}>
+        <NativeRouter>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/login" element={<LoginScreen />} />
+            <Route path="/exercises" element={<ExercisesScreen />} />
+            <Route path="/exercise/:id" element={<ExerciseScreen />} />
+          </Routes>
+        </NativeRouter>
+      </QueryClientProvider>
+    </PaperProvider>
   );
 };
 
