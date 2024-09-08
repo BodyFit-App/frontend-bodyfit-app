6245ac4a3c3190e748af95323ea3cd136d651098
Nicolas-B06 <bernardnicolas06600@gmail.com>
2024-09-08 22:28:04 +0200

    Add jsDoc

--- components/ObjectifCard/ObjectifCard.tsx
+++ components/ObjectifCard/ObjectifCard.tsx
@@ -1,6 +1,36 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import { Card, Text, ProgressBar } from "react-native-paper";
+/**
+ * ObjectifCard Component
+ *
+ * Ce composant représente une carte d'objectif, qui affiche un titre, une période (date de début et de fin),
+ * une description, ainsi qu'une barre de progression indiquant l'avancement de l'objectif.
+ * Il est possible d'ajouter une action lorsqu'on clique sur la carte via la prop `onPress`.
+ *
+ * @component
+ * @example
+ * return (
+ *   <ObjectifCard
+ *     title="Perte de poids"
+ *     startDate="01/01/2024"
+ *     endDate="31/12/2024"
+ *     description="Objectif de perte de poids pour l'année 2024."
+ *     progress={0.4}
+ *     onPress={() => console.log('Accéder à l\'objectif')}
+ *   />
+ * );
+ *
+ * @param {Object} props - Les propriétés du composant.
+ * @param {string} props.title - Le titre de l'objectif.
+ * @param {string} props.startDate - La date de début de l'objectif.
+ * @param {string} props.endDate - La date de fin de l'objectif.
+ * @param {string} props.description - La description de l'objectif. Si la description dépasse 100 caractères, elle est tronquée avec "...".
+ * @param {number} props.progress - La progression de l'objectif, exprimée sous forme de nombre entre 0 et 1 (ex. 0.5 pour 50%).
+ * @param {function} [props.onPress] - Fonction optionnelle appelée lorsque la carte est pressée.
+ *
+ * @returns {JSX.Element} Un élément React représentant une carte d'objectif avec titre, dates, description et progression.
+ */
 
 interface ObjectifCardProps {
   title: string;
