d68a6ca398ceb3ae191f5df127cdd2d2e80cc0f2
stephane0713 <stephaned2205@gmail.com>
2024-09-12 11:18:53 +0200

    fix: tests were in wrong file

--- tests/helpers.test.ts
+++ tests/helpers.test.ts
@@ -1,4 +1,9 @@
-import { getRange } from "../lib/helpers";
+import {
+  formatExercisesWithFavorites,
+  formatProgramsWithFavorites,
+  getRange,
+} from "../lib/helpers";
+import { Tables } from "../types/database.types";
 
 describe("Test helpers", () => {
   describe("getRange", () => {
@@ -15,5 +20,221 @@ describe("Test helpers", () => {
         expect(result).toEqual(expected);
       },
     );
+  });
+});
+
+describe("formatProgramsWithFavorites", () => {
+  it("should correctly mark programs as favorites", () => {
+    const programs = [
+      {
+        id: 1,
+        title: "Fitness Program",
+        profile_id: 1,
+        created_at: "2000-12-20",
+        description: "Exemple",
+        visible: true,
+      },
+      {
+        id: 2,
+        title: "Yoga Program",
+        profile_id: 2,
+        created_at: "2001-05-10",
+        description: "Exemple",
+        visible: false,
+      },
+    ];
+    const favorites = [1];
+    const nextCursor = null;
+
+    const result = formatProgramsWithFavorites(programs, favorites, nextCursor);
+
+    expect(result).toEqual({
+      nextCursor: null,
+      programs: [
+        {
+          id: 1,
+          title: "Fitness Program",
+          profile_id: 1,
+          created_at: "2000-12-20",
+          description: "Exemple",
+          visible: true,
+          isFav: true,
+        },
+        {
+          id: 2,
+          title: "Yoga Program",
+          profile_id: 2,
+          created_at: "2001-05-10",
+          description: "Exemple",
+          visible: false,
+          isFav: false,
+        },
+      ],
+    });
+  });
+
+  it("should return programs with correct nextCursor", () => {
+    const programs = [
+      {
+        id: 1,
+        title: "Fitness Program",
+        profile_id: 1,
+        created_at: "2000-12-20",
+        description: "Exemple",
+        visible: true,
+      },
+    ];
+    const favorites: any = [];
+    const nextCursor = 456;
+
+    const result = formatProgramsWithFavorites(programs, favorites, nextCursor);
+
+    expect(result).toEqual({
+      nextCursor: 456,
+      programs: [
+        {
+          id: 1,
+          title: "Fitness Program",
+          profile_id: 1,
+          created_at: "2000-12-20",
+          description: "Exemple",
+          visible: true,
+          isFav: false,
+        },
+      ],
+    });
+  });
+
+  it("should handle empty programs array", () => {
+    const programs: Tables<"programs">[] = [];
+    const favorites = [1, 2, 3];
+    const nextCursor = null;
+
+    const result = formatProgramsWithFavorites(programs, favorites, nextCursor);
+
+    expect(result).toEqual({
+      nextCursor: null,
+      programs: [],
+    });
+  });
+});
+
+describe("formatExercisesWithFavorites", () => {
+  it("should correctly mark exercises as favorites", () => {
+    const exercises = [
+      {
+        id: 1,
+        title: "Pull Up",
+        description: "Example",
+        profile_id: 1,
+        created_at: "2000-10-20",
+        visible: true,
+        banner_image: null,
+        estimated_time_seconds: null,
+      },
+      {
+        id: 2,
+        title: "Push Up",
+        description: "Example",
+        profile_id: 2,
+        created_at: "2001-01-15",
+        visible: true,
+        banner_image: null,
+        estimated_time_seconds: null,
+      },
+    ];
+    const favorites = [1];
+    const nextCursor = null;
+
+    const result = formatExercisesWithFavorites(
+      exercises,
+      favorites,
+      nextCursor,
+    );
+
+    expect(result).toEqual({
+      nextCursor: null,
+      exercises: [
+        {
+          id: 1,
+          title: "Pull Up",
+          description: "Example",
+          profile_id: 1,
+          created_at: "2000-10-20",
+          visible: true,
+          banner_image: null,
+          estimated_time_seconds: null,
+          isFav: true,
+        },
+        {
+          id: 2,
+          title: "Push Up",
+          description: "Example",
+          profile_id: 2,
+          created_at: "2001-01-15",
+          visible: true,
+          banner_image: null,
+          estimated_time_seconds: null,
+          isFav: false,
+        },
+      ],
+    });
+  });
+
+  it("should return exercises with correct nextCursor", () => {
+    const exercises = [
+      {
+        id: 1,
+        title: "Pull Up",
+        description: "Example",
+        profile_id: 1,
+        created_at: "2000-10-20",
+        visible: true,
+        banner_image: null,
+        estimated_time_seconds: null,
+      },
+    ];
+    const favorites: any = [];
+    const nextCursor = 123;
+
+    const result = formatExercisesWithFavorites(
+      exercises,
+      favorites,
+      nextCursor,
+    );
+
+    expect(result).toEqual({
+      nextCursor: 123,
+      exercises: [
+        {
+          id: 1,
+          title: "Pull Up",
+          description: "Example",
+          profile_id: 1,
+          created_at: "2000-10-20",
+          visible: true,
+          banner_image: null,
+          estimated_time_seconds: null,
+          isFav: false,
+        },
+      ],
+    });
+  });
+
+  it("should handle empty exercises array", () => {
+    const exercises: Tables<"exercises">[] = [];
+    const favorites = [1, 2, 3];
+    const nextCursor = null;
+
+    const result = formatExercisesWithFavorites(
+      exercises,
+      favorites,
+      nextCursor,
+    );
+
+    expect(result).toEqual({
+      nextCursor: null,
+      exercises: [],
+    });
   });
 });
