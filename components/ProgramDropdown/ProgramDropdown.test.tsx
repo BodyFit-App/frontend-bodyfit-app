5759901ed52f32dc2c4d201544eed5d4e31521ce
stephane0713 <stephaned2205@gmail.com>
2024-09-17 11:26:29 +0200

    added test

--- components/ProgramDropdown/ProgramDropdown.test.tsx
+++ components/ProgramDropdown/ProgramDropdown.test.tsx
@@ -1,44 +1,43 @@
 import React from "react";
 import { render, waitFor } from "@testing-library/react-native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import ExerciseDropdown from "./ProgramDropdown";
-import { fetchDropdownExercises } from "../../api/exercises";
-import { MultiSelectDropdown } from "react-native-paper-dropdown";
-import DropdownInput from "../DropdownInput/DropdownInput";
+import { Dropdown } from "react-native-paper-dropdown";
+import { fetchDropdownPrograms } from "../../api/programs";
+import ProgramDropdown from "./ProgramDropdown";
 
-jest.mock("../../api/exercises", () => ({
-  fetchDropdownExercises: jest.fn(),
+jest.mock("../../api/programs", () => ({
+  fetchDropdownPrograms: jest.fn(),
 }));
 
 jest.mock("react-native-paper-dropdown", () => ({
-  MultiSelectDropdown: jest.fn(() => null),
+  Dropdown: jest.fn(() => null),
 }));
 
 jest.mock("../DropdownInput/DropdownInput", () => "DropdownInput");
 
-describe("ExerciseDropdown", () => {
+describe("ProgramDropdown", () => {
   const queryClient = new QueryClient();
 
-  it("should generate the correct options from fetchDropdownExercises", async () => {
-    const mockExercises = [
-      { id: 1, title: "Exercise 1" },
-      { id: 2, title: "Exercise 2" },
+  it("should generate the correct options from fetchDropdownPrograms", async () => {
+    const mockPrograms = [
+      { id: 1, title: "Program 1" },
+      { id: 2, title: "Program 2" },
     ];
 
-    (fetchDropdownExercises as jest.Mock).mockResolvedValue(mockExercises);
+    (fetchDropdownPrograms as jest.Mock).mockResolvedValue(mockPrograms);
 
     render(
       <QueryClientProvider client={queryClient}>
-        <ExerciseDropdown value={[]} onChange={() => {}} />
+        <ProgramDropdown value={null} onChange={() => {}} />
       </QueryClientProvider>
     );
 
     await waitFor(() => {
-      expect(MultiSelectDropdown).toHaveBeenCalledWith(
+      expect(Dropdown).toHaveBeenCalledWith(
         expect.objectContaining({
           options: [
-            { label: "Exercise 1", value: "1" },
-            { label: "Exercise 2", value: "2" },
+            { label: "Program 1", value: "1" },
+            { label: "Program 2", value: "2" },
           ],
         }),
         {}
